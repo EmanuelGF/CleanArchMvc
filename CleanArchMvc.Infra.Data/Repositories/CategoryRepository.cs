deb352286bbc68bb2e3bcf66056dedcb4769fbe2
EmanuelGF <emanuelfaisca@gmail.com>
2021-09-30 21:28:04 +0100

    Added the db context to Infra.Data; Cofigured the entities using fluent api; Implemented the repository pattern for data communication.

---
+++ CleanArchMvc.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,54 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    class CategoryRepository : ICategoryRepository
+    {
+        //ctor
+        //Implementing dependency injection
+        private ApplicationDbContext _categoryContext;
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _categoryContext = context;
+
+        }
+
+        //methods
+        public async Task<Category> CreateAsync(Category category)
+        {
+            _categoryContext.Add(category);
+            await _categoryContext.SaveChangesAsync(); //This always needs to be called.
+            return category;
+        }
+
+        public async Task<Category> GetByIdAsync(int? id)
+        {
+            return await _categoryContext.Categories.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Category>> GetCategoriesAsync()
+        {
+            return await _categoryContext.Categories.ToListAsync();
+        }
+
+        public async Task<Category> RemoveAsync(Category category)
+        {
+            _categoryContext.Remove(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> UpdateAsync(Category category)
+        {
+            _categoryContext.Update(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+    }
+}
