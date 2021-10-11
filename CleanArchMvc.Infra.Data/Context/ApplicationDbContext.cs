54fee6d5378cc54328ca0f2af26d5e1035b39807
EmanuelGF <emanuelfaisca@gmail.com>
2021-10-11 18:03:58 +0100

    added Identity packages and created the Identity db using migrations.

--- CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
+++ CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Infra.Data.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -23,7 +25,7 @@ Microsoft.EntityFrameworkCore.Tools
 */
 namespace CleanArchMvc.Infra.Data.Context
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         //ctor
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
